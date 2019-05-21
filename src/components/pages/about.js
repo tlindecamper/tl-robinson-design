172de729dc1b5d89e5be521f61da93022c1fca88
tlindecamper <therelan2001@yahoo.com>
2019-05-21 09:38:48 -0600

    media queries

--- src/components/pages/about.js
+++ src/components/pages/about.js
@@ -7,15 +7,15 @@ export default function About() {
                 <h1 >About Us</h1>
             </div> 
             
-                <h1  className="mission-statement">I want to earn my living by doing quality design work in a way that will serve myself and my Client by
+                <h1  className="mission-statement">I want to earn my living by doing quality design work in a way that will serve my Client and Myself by
                      assisiting them in developing the full potential of their surroundings through beauty, balance and individual uniqueness.</h1>
              
 
-                <img   src="assets/residential-gallery/vwsundance/vw2.jpg" />
+                <img className="about-pic"   src="assets/residential-gallery/vwsundance/vw2.jpg" />
             
                 <h2  className="services" >We provide many services to meet all of your needs. <br/> 
                     Custom Drapery and Upholstery<br/> 
-                    Uniqe yet functional Design Ideas for Home or Business <br/> 
+                    Unique yet functional Design Ideas for Home or Business <br/> 
                     Seasonal Decorating<br/> 
                     Custom Landscaping, Native Landscaping and Maintenance<br/> 
                     Re-Furbishing Furniture and Finishing Projects<br/>
